4cad52e17c0b6de7ac8ebc879bde005b4922af63
shaikhusama155 <shaikhusama155@gmail.com>
2023-04-13 18:33:56 +0500

    assignment

---
+++ script.js
@@ -0,0 +1,14 @@
+let number = parseFloat(prompt("Enter positive number"))
+document.write(`<p class="fprogram"><span>Program 1</span><br><br>
+    Number: ${number} <br>
+    Round off value: ${Math.round(number)} <br> 
+    Floor value: ${Math.floor(number)} <br>
+    Ceil value: ${Math.ceil(number)} </p> `)
+
+
+let negitiveNumber = parseFloat(prompt("Enter positive number"))
+document.write(`<p class="sprogram"><span class = "sspan">Program 1</span><br><br>
+    Number: ${number} <br>
+    Round off value: ${Math.round(negitiveNumber)} <br> 
+    Floor value: ${Math.floor(negitiveNumber)} <br>
+    Ceil value: ${Math.ceil(negitiveNumber)} </p> `)
